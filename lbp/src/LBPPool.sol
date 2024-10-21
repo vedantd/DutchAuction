1f925b4691eed72c3b9e5b4ba5ca1481185b4d9e
Vedant Dalvi <vedantdalvi20@gmail.com>
2024-10-21 15:38:00 -0400

    fix import

--- lbp/src/LBPPool.sol
+++ lbp/src/LBPPool.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelinu/contracts/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
